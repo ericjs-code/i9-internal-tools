c80f9b6e4cc06b12efb01f7f135334bc9e6a2fe9
Vinicius Stoc <vmartinsstoc@gmail.com>
2026-03-13 16:42:23 -0300

    UPLOAD/ Criação dashboard compras

--- compras/urls.py
+++ compras/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/upload-dw/', views.api_upload_compras, name='api_upload_compras'),
+    path('dashboard/', views.dashboard_compras, name='dashboard_compras')
 ]
--- compras/views.py
+++ compras/views.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from django.http import JsonResponse
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Sum, Avg, Count, F, Q
 from .models import DataWarehouseCompras
 
 
@@ -12,7 +16,7 @@ def api_upload_compras(request):
     if request.method == 'POST' and request.FILES.get('arquivo'):
 
         token = request.headers.get('X-Api-Key')
-        if token != 'I9TMG_CHAVE_SECRETA_2026':
+        if token != 'l_^e1#ye7@wro)4@gti24vxcmrr$01(@sxdp@=qg40(^vkvwzr':
             return JsonResponse({'erro': 'Acesso negado. Token inválido.'}, status=403)
 
         arquivo = request.FILES['arquivo']
@@ -65,3 +69,52 @@ def api_upload_compras(request):
             return JsonResponse({'erro': f'Falha no processamento: {str(e)}'}, status=500)
 
     return JsonResponse({'erro': 'Requisição inválida ou sem arquivo.'}, status=400)
+
+@login_required(login_url='/login/')
+def dashboard_compras(request):
+    if not (request.user.is_superuser or getattr(request.user, 'is_compras', False) or getattr(request.user,'is_diretoria', False) or getattr(
+            request.user, 'is_ti', False)):
+        messages.error(request, "Acesso restrito à Diretoria e equipe de Compras.")
+        return redirect('home')
+
+    pedidos_efetivados = DataWarehouseCompras.objects.exclude(status='Pendente')
+
+    spend_total = pedidos_efetivados.aggregate(total=Sum('valor_total'))['total'] or 0.0
+
+    curva_abc_projetos = pedidos_efetivados.exclude(projeto_cod='').values('projeto_cod').annotate(
+        custo_total=Sum('valor_total')
+    ).order_by('-custo_total')[:5]
+
+
+    lead_time_compras = pedidos_efetivados.aggregate(media=Avg('leadtime_compras'))['media'] or 0.0
+
+    backlog_sc = DataWarehouseCompras.objects.filter(status='PENDENTE').count()
+
+    pedidos_entregues = DataWarehouseCompras.objects.filter(status='ENTREGUE')
+
+    atraso_medio_fornecedores = pedidos_entregues.aggregate(media=Avg('dias_atraso_entrega'))['media'] or 0.0
+
+    piores_fornecedores = pedidos_entregues.exclude(nome_fornecedor='').values('nome_fornecedor').annotate(
+        media_atraso=Avg('dias_atraso_entrega'),
+        volume_pedidos=Count('id')
+    ).filter(volume_pedidos__gte=3).order_by('-media_atraso')[:5]
+
+    projetos_labels = [p['projeto_cod'] for p in curva_abc_projetos]
+    projetos_data = [float(p['custo_total']) for p in curva_abc_projetos]
+
+    fornecedores_labels = [f['nome_fornecedor'][:15] + '...' if len(f['nome_fornecedor']) > 15 else f['nome_fornecedor'] for f in piores_fornecedores]
+    fornecedores_data = [float(f['media_atraso']) for f in piores_fornecedores]
+
+    context = {
+        'spend_total': spend_total,
+        'lead_time_compras': round(lead_time_compras, 1),
+        'backlog_sc': backlog_sc,
+        'atraso_medio_fornecedores': round(atraso_medio_fornecedores, 1),
+
+        'projetos_labels': projetos_labels,
+        'projetos_data': projetos_data,
+        'fornecedores_labels': fornecedores_labels,
+        'fornecedores_data': fornecedores_data,
+    }
+
+    return render(request, 'compras/dashboard.html', context)
